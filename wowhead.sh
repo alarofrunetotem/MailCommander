c5ba9fb886b655a4ad9a9b9b385763c680e9defe
Alar of Daggerspine <alar@aspide.it>
2016-05-24 11:54:24 +0200

    Added pet stones classes

--- wowhead.sh
+++ wowhead.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)
 echo -e "local me,ns = ...\n" >wowhead.lua
+date +"ns.wowhead_update=%s" >>wowhead.lua
 ../wowhelpers/MCClassBoa.php >>wowhead.lua
-#../wowhelpers/GCGearTokens.php >>wowhead.lua
+../wowhelpers/MCBattlestones.php >>wowhead.lua

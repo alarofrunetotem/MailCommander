7bc7e6d1bcff40a6e1a61e143a44dd420738c0d7
Alar of Runetotem <alar@aspide.it>
2017-03-28 08:59:07 +0200

    Updated class list (added demon hunter)

--- wowhead.lua
+++ wowhead.lua
@@ -1,149 +1,188 @@
 local me,ns = ...
 
-ns.wowhead_update=1464071579
+ns.wowhead_update=1487324926
 -- Classes masks
 ns.classes=
 {
-	PALADIN = {
-		id = 2,
-		enUS = "Paladin",
-		mask = 2
-	},
-	SHAMAN = {
-		id = 7,
-		enUS = "Shaman",
-		mask = 64
-	},
-	MAGE = {
-		id = 8,
-		enUS = "Mage",
-		mask = 128
-	},
-	WARRIOR = {
-		id = 1,
-		enUS = "Warrior",
-		mask = 1
-	},
-	DRUID = {
-		id = 11,
-		enUS = "Druid",
-		mask = 1024
-	},
-	WARLOCK = {
-		id = 9,
-		enUS = "Warlock",
-		mask = 256
-	},
-	PRIEST = {
-		id = 5,
-		enUS = "Priest",
-		mask = 16
-	},
-	MONK = {
-		id = 10,
-		enUS = "Monk",
-		mask = 512
-	},
-	ROGUE = {
-		id = 4,
-		enUS = "Rogue",
-		mask = 8
-	},
-	DEATHKNIGHT = {
-		id = 6,
-		enUS = "Death Knight",
-		mask = 32
-	},
-	HUNTER = {
-		id = 3,
-		enUS = "Hunter",
-		mask = 4
-	}
+  DEATHKNIGHT = {
+    id = 6,
+    mask = 32,
+    enUS = "Death Knight"
+  },
+  WARRIOR = {
+    id = 1,
+    mask = 1,
+    enUS = "Warrior"
+  },
+  ROGUE = {
+    id = 4,
+    mask = 8,
+    enUS = "Rogue"
+  },
+  MAGE = {
+    id = 8,
+    mask = 128,
+    enUS = "Mage"
+  },
+  PRIEST = {
+    id = 5,
+    mask = 16,
+    enUS = "Priest"
+  },
+  HUNTER = {
+    id = 3,
+    mask = 4,
+    enUS = "Hunter"
+  },
+  WARLOCK = {
+    id = 9,
+    mask = 256,
+    enUS = "Warlock"
+  },
+  DEMONHUNTER = {
+    id = 12,
+    mask = 2048,
+    enUS = "Demon Hunter"
+  },
+  SHAMAN = {
+    id = 7,
+    mask = 64,
+    enUS = "Shaman"
+  },
+  DRUID = {
+    id = 11,
+    mask = 1024,
+    enUS = "Druid"
+  },
+  MONK = {
+    id = 10,
+    mask = 512,
+    enUS = "Monk"
+  },
+  PALADIN = {
+    id = 2,
+    mask = 2,
+    enUS = "Paladin"
+  }
 }
 
--- DataMined from WowHead items?filter=cr=133:152;crs=1:6;crv=0:0 on 24/05/2016
+-- DataMined from WowHead items?filter=cr=133:152;crs=1:6;crv=0:0 on 17/02/2017
 -- Scraped 16 items
--- DataMined from WowHead items?filter=cr=133:152;crs=1:11;crv=0:0 on 24/05/2016
+-- DataMined from WowHead items?filter=cr=133:152;crs=1:11;crv=0:0 on 17/02/2017
 -- Scraped 18 items
--- DataMined from WowHead items?filter=cr=133:152;crs=1:3;crv=0:0 on 24/05/2016
+-- DataMined from WowHead items?filter=cr=133:152;crs=1:3;crv=0:0 on 17/02/2017
 -- Scraped 16 items
--- DataMined from WowHead items?filter=cr=133:152;crs=1:8;crv=0:0 on 24/05/2016
+-- DataMined from WowHead items?filter=cr=133:152;crs=1:8;crv=0:0 on 17/02/2017
 -- Scraped 17 items
 ns.classBoa=
 {
-	["127781"] = 400,
-	["128473"] = 400,
-	["102322"] = 1544,
-	["102274"] = 68,
-	["127796"] = 1544,
-	["127778"] = 400,
-	["128472"] = 1544,
-	["102263"] = 35,
-	["102275"] = 68,
-	["127805"] = 68,
-	["127777"] = 400,
-	["127817"] = 35,
-	["102280"] = 1544,
-	["102272"] = 68,
-	["127795"] = 1544,
-	["102290"] = 400,
-	["102288"] = 400,
-	["102284"] = 400,
-	["127809"] = 68,
-	["102277"] = 1544,
-	["102271"] = 68,
-	["102264"] = 35,
-	["102289"] = 400,
-	["127780"] = 400,
-	["127820"] = 35,
-	["102276"] = 68,
-	["127793"] = 1544,
-	["102282"] = 1544,
-	["127819"] = 35,
-	["127806"] = 68,
-	["127779"] = 400,
-	["127803"] = 68,
-	["127783"] = 400,
-	["127808"] = 68,
-	["127784"] = 400,
-	["127807"] = 68,
-	["102281"] = 1544,
-	["127797"] = 1544,
-	["127816"] = 35,
-	["102278"] = 1544,
-	_lastupdate = 1464071579,
-	["102321"] = 400,
-	["102273"] = 68,
-	["102279"] = 1544,
-	["102287"] = 400,
-	["127790"] = 1544,
-	["127822"] = 35,
-	["102323"] = 68,
-	["127791"] = 1544,
-	["102283"] = 1544,
-	["102285"] = 400,
-	["127810"] = 68,
-	["127794"] = 1544,
-	["102320"] = 35,
-	["102268"] = 35,
-	["127792"] = 1544,
-	["102267"] = 35,
-	["127782"] = 400,
-	["127821"] = 35,
-	["127823"] = 35,
-	["102265"] = 35,
-	["102270"] = 68,
-	["127804"] = 68,
-	["102269"] = 35,
-	["94604"] = 1024,
-	["102266"] = 35,
-	["102286"] = 400,
-	["127818"] = 35
+  ["102290"] = 400,
+  ["102280"] = 1544,
+  ["127816"] = 35,
+  ["127790"] = 3592,
+  ["127782"] = 400,
+  ["102269"] = 35,
+  ["102322"] = 1544,
+  ["127805"] = 68,
+  ["102274"] = 68,
+  ["127795"] = 3592,
+  ["127821"] = 35,
+  ["127780"] = 400,
+  ["128472"] = 1544,
+  ["127792"] = 3592,
+  ["102284"] = 400,
+  ["102270"] = 68,
+  ["127783"] = 400,
+  ["127822"] = 35,
+  ["127818"] = 35,
+  ["127803"] = 68,
+  ["127781"] = 400,
+  ["127809"] = 68,
+  ["102273"] = 68,
+  ["127823"] = 35,
+  ["102263"] = 35,
+  ["127820"] = 35,
+  ["102264"] = 35,
+  ["102277"] = 1544,
+  ["127779"] = 400,
+  ["127817"] = 35,
+  ["127808"] = 68,
+  ["102266"] = 35,
+  ["102279"] = 1544,
+  ["127778"] = 400,
+  ["127793"] = 3592,
+  ["102283"] = 1544,
+  ["127794"] = 3592,
+  ["102268"] = 35,
+  ["102271"] = 68,
+  ["102275"] = 68,
+  ["102320"] = 35,
+  ["102265"] = 35,
+  ["127797"] = 3592,
+  ["102282"] = 1544,
+  ["127791"] = 3592,
+  ["102267"] = 35,
+  ["102323"] = 68,
+  ["128473"] = 400,
+  ["102321"] = 400,
+  ["94604"] = 1024,
+  ["102289"] = 400,
+  ["102288"] = 400,
+  ["127807"] = 68,
+  ["102287"] = 400,
+  ["102286"] = 400,
+  ["127806"] = 68,
+  ["102281"] = 1544,
+  ["127784"] = 400,
+  ["102272"] = 68,
+  ["127819"] = 35,
+  ["127804"] = 68,
+  ["127810"] = 68,
+  ["127777"] = 400,
+  ["102278"] = 1544,
+  ["102276"] = 68,
+  ["102285"] = 400,
+  ["127796"] = 3592
 }
 
--- DataMined from WowHead items?filter=na=battlestone;cr=133;crs=1;crv=0 on 24/05/2016
--- Scraped 11 items
-ns.battlestones={92665,92675,92676,92677,92678,92679,92680,92681,92682,92683,98715}
--- DataMined from WowHead items?filter=na=training;cr=133;crs=1;crv=0 on 24/05/2016
+-- DataMined from WowHead items?filter=na=battlestone;cr=133;crs=1;crv=0 on 17/02/2017
+-- Scraped 21 items
+ns.battlestones={
+  137387, -- 4Immaculate Dragonkin Battle-Stone
+  137388, -- 4Immaculate Humanoid Battle-Stone
+  137389, -- 4Immaculate Undead Battle-Stone
+  137390, -- 4Immaculate Mechanical Battle-Stone
+  137391, -- 4Immaculate Aquatic Battle-Stone
+  137392, -- 4Immaculate Magic Battle-Stone
+  137393, -- 4Immaculate Critter Battle-Stone
+  137394, -- 4Immaculate Beast Battle-Stone
+  137395, -- 4Immaculate Elemental Battle-Stone
+  137396, -- 4Immaculate Flying Battle-Stone
+  92665, -- 5Flawless Elemental Battle-Stone
+  92675, -- 5Flawless Beast Battle-Stone
+  92676, -- 5Flawless Critter Battle-Stone
+  92677, -- 5Flawless Flying Battle-Stone
+  92678, -- 5Flawless Magic Battle-Stone
+  92679, -- 5Flawless Aquatic Battle-Stone
+  92680, -- 5Flawless Mechanical Battle-Stone
+  92681, -- 5Flawless Undead Battle-Stone
+  92682, -- 5Flawless Humanoid Battle-Stone
+  92683, -- 5Flawless Dragonkin Battle-Stone
+  98715, -- 5Marked Flawless Battle-Stone
+}
+-- DataMined from WowHead items?filter=na=training;cr=133;crs=1;crv=0 on 17/02/2017
 -- Scraped 12 items
-ns.trainingstones={116374,116416,116417,116418,116419,116420,116421,116422,116423,116424,116429,127755}
+ns.trainingstones={
+  116374, -- 7Beast Battle-Training Stone
+  116416, -- 7Humanoid Battle-Training Stone
+  116417, -- 7Mechanical Battle-Training Stone
+  116418, -- 7Critter Battle-Training Stone
+  116419, -- 7Dragonkin Battle-Training Stone
+  116420, -- 7Elemental Battle-Training Stone
+  116421, -- 7Flying Battle-Training Stone
+  116422, -- 7Magic Battle-Training Stone
+  116423, -- 7Undead Battle-Training Stone
+  116424, -- 7Aquatic Battle-Training Stone
+  116429, -- 7Flawless Battle-Training Stone
+  127755, -- 7Fel-Touched Battle-Training Stone
+}

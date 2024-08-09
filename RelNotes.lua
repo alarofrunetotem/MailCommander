4970472f590b0ef4774155326915cc892cfb31f8
Alar of Runetotem <alar@aspide.it>
2024-08-09 21:59:38 +0200

    Toc: Updated to 11.0.0, 11.0.2

--- RelNotes.lua
+++ RelNotes.lua
@@ -8,7 +8,9 @@ self:Wiki([[
 Mail Commander allows you to define a per character list of needs which wil be displayed when you open the sendmail panel allowing you to send all requested items or just some of them.
 You can make this selection permament, disabling some kind of items, or just cherry pick each time
 You can also use the "Send all" button to have all enabled items sent with a single click
-
+]])
+self:RelNotes(2,6,0,[[
+  Toc: Updated to 11.0.0, 11.0.2
 ]])
 self:RelNotes(2,5,1,[[
   Toc: Updated to 10.2.7

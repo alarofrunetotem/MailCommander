7251755a105abbdd3df4c2faef8f583c49b761b8
Alar of Runetotem <alar@aspide.it>
2026-01-23 15:40:01 +0100

    Version 2.9.1

--- RelNotes.lua
+++ RelNotes.lua
@@ -11,6 +11,12 @@ You can make this selection permament, disabling some kind of items, or just che
 You can also use the "Send all" button to have all enabled items sent with a single click
 ]])
 self:HF_Paragraph('Release Notes')
+self:RelNotes(2,10,0,[[
+Fix: error on start
+]])
+self:RelNotes(2,9,1,[[
+Fix: error on start
+]])
 self:RelNotes(2,9,0,[[
 Toc: 11.2.7
 ]])
